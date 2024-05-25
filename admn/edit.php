753362ff3a4fce04e9c0bcdf01a050381b5c7658
aswinsadhan <109092901+aswinsadhan@users.noreply.github.com>
2024-05-25 15:42:42 +0530

    Add files via upload

--- admn/edit.php
+++ admn/edit.php
@@ -1,5 +1,5 @@
 <?php
-$Title = 'Dashboard | Edit Courier Details'; 
+$Title = 'Dashboard | Edit Vendor Details'; 
 include("../../config/all_config.php"); 
 include("../../lib/all_lib.php"); 
 include("../../partials/dashboard_header.php"); 
@@ -14,21 +14,22 @@ if(empty($_GET['id'])){
 }
 
 $id = $_GET['id'];
+
 ?>
-<h1> Edit Courier Details </h1>
+<h1> Edit Vendor Details </h1>
 <br>
 
 <?php
 
-$name_input          = new Input("courier_name","Courier Name",30,5);
-$building_name_input = new Input("courier_building_name","Builing Name",20,5);
-$street_input        = new Input("courier_street","Street/Area",20,5);
-$city_input          = new Input("courier_city","City",20,5);
-$state_input         = new Input("courier_state", "State");
+$name_input          = new Input("vendor_name","Vendor Name",30,5);
+$building_name_input = new Input("vendor_building_name","Builing Name",20,5);
+$street_input        = new Input("vendor_street","Street/Area",20,5);
+$city_input          = new Input("vendor_city","City",20,5);
+$state_input         = new Input("vendor_state", "State");
 $state_input->type = "select";
 $state_input->selectOptions = INDIAN_STATES;
-$pincode_input    = new Input("courier_pincode","Pincode",6,3);
-$phone_input      = new Input("courier_phone","Phone",10,8);
+$pincode_input    = new Input("vendor_pincode","Pincode",6,3);
+$phone_input      = new Input("vendor_phone","Phone",10,8);
 
 $form = new Form(
     $name_input,
@@ -40,16 +41,16 @@ $form = new Form(
     $pincode_input,
 );
 
-$form->sql_table = "tbl_courier";
+$form->sql_table = "tbl_vendor";
 $form->sql_id = $id;
 $form->sql_id_type = "i"; 
-$form->sql_pk_name = "courier_id"; 
+$form->sql_pk_name = "vendor_id"; 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if($form->validate()) {
         $form->save();
-        Messages::add("success","Courier '{$name_input->value}' was edited successfully!");
-        redirect("/admin/couriers/");
+        Messages::add("success","Vendor '{$name_input->value}' was edited successfully!");
+        redirect("/admin/vendor/");
     } 
 }
 
